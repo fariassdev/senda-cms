13e3efd8f0a02d5856a68d6b982eaac4df6ac666
Fernando Arias <fernan.fpinformatica@gmail.com>
2025-09-29 22:33:59 +0200

    feat: add shorthand TypeScript types based on OpenAPI generated types

---
+++ src/types/models.ts
@@ -0,0 +1,7 @@
+import type { components } from '@/types/api';
+
+export type Course = components['schemas']['Course'];
+export type Lesson = components['schemas']['Lesson'];
+export type LessonStatus = components['schemas']['LessonStatus'];
+export type ScriptPart = components['schemas']['ScriptPart'];
+export type User = components['schemas']['UserPublic'];
